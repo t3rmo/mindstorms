91fd321b7945ce585b07aa95f5311cf6b1e71112
t3rmo <rauh.maximilian@gmail.com>
2019-12-19 12:22:05 +0100

    created function for responding

--- mindstormsFunction/src/mindstormsFunction/Function.cs
+++ mindstormsFunction/src/mindstormsFunction/Function.cs
@@ -22,15 +22,29 @@ namespace mindstormsFunction
     {
         public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
         {
+
+            if (input.Request is LaunchRequest)
+            {
+                return createAnswer("Der skill wurder erfolgreich gestartet!");
+            }
+            else
+            {
+                return createAnswer("Der Skill wurde nicht über den LaunchRequest aufgerufen!");
+            }
+
+        }
+
+        private SkillResponse createAnswer(String answer = "Hey, das ist ein Test", String bodyTitle = "Debugging", String content = "Debugging and Testing Alexa")
+        {
             // create the speech response
             var speech = new SsmlOutputSpeech();
-            speech.Ssml = "<speak>This is an test.</speak>";
+            speech.Ssml = $"<speak>{answer}</speak>";
 
             // create the response
             var responseBody = new ResponseBody();
             responseBody.OutputSpeech = speech;
             responseBody.ShouldEndSession = true; // this triggers the reprompt
-            responseBody.Card = new SimpleCard { Title = "Test", Content = "Testing Alexa" };
+            responseBody.Card = new SimpleCard { Title = bodyTitle, Content = content };
 
             var skillResponse = new SkillResponse();
             skillResponse.Response = responseBody;
@@ -39,4 +53,5 @@ namespace mindstormsFunction
             return skillResponse;
         }
     }
+
 }

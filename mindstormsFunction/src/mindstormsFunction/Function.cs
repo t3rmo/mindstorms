879a21f639d61a4b849bf119aab7c498a7c0ae0d
t3rmo <rauh.maximilian@gmail.com>
2019-12-19 14:55:56 +0100

    custom interface added

--- mindstormsFunction/src/mindstormsFunction/Function.cs
+++ mindstormsFunction/src/mindstormsFunction/Function.cs
@@ -12,6 +12,7 @@ using Alexa.NET.Response;
 using Alexa.NET.Request;
 using Alexa.NET.Request.Type;
 using Alexa.NET;
+using Alexa.NET.Gadgets.CustomInterfaces;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -20,21 +21,42 @@ namespace mindstormsFunction
 {
     public class Function
     {
-        public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
+        public async Task<SkillResponse> FunctionHandler(SkillRequest input, ILambdaContext context)
         {
+            //Anfrage bearbeiten
+            EndpointApi api = new EndpointApi(input);
+            var endpoints = await api.GetEndpoints();
+
+            if (endpoints.Endpoints.Length <= 0)
+            {
+                return createAnswer("Ich konnte keine Verbundenen Geräte finden.");
+            }
+            else if (endpoints.Endpoints.Length >= 0)
+            {
+                return createAnswer("Es wurden Geräte gefunden, mit denen ich Verbunden bin!");
+            }
 
             if (input.Request is LaunchRequest)
             {
                 return createAnswer("Der skill wurder erfolgreich gestartet!");
             }
+            else if (input.Request is IntentRequest)
+            {
+                IntentRequest intent = input.Request as IntentRequest;
+                if (intent.Intent.Name.Equals("SetSpeedIntent"))
+                {
+                    return createAnswer("Geschwindigkeit wurde gesetzt!");
+                }
+                return createAnswer("Es wurde ein nicht programmierter Intent aufgerufen! " + intent.Intent.Name);
+            }
             else
             {
-                return createAnswer("Der Skill wurde nicht über den LaunchRequest aufgerufen!");
+                return createAnswer("Der Skill wurde nicht über einen LaunchRequest oder IntentRequest gestartet.", true);
             }
 
         }
 
-        private SkillResponse createAnswer(String answer = "Hey, das ist ein Test", String bodyTitle = "Debugging", String content = "Debugging and Testing Alexa")
+        private SkillResponse createAnswer(String answer = "Hey, das ist ein Test", bool endSession = false, String repromptText = "Ich höre zu!", String bodyTitle = "Debugging", String content = "Debugging and Testing Alexa")
         {
             // create the speech response
             var speech = new SsmlOutputSpeech();
@@ -43,7 +65,8 @@ namespace mindstormsFunction
             // create the response
             var responseBody = new ResponseBody();
             responseBody.OutputSpeech = speech;
-            responseBody.ShouldEndSession = true; // this triggers the reprompt
+            responseBody.ShouldEndSession = endSession; // this triggers the reprompt
+            responseBody.Reprompt = new Reprompt(repromptText);
             responseBody.Card = new SimpleCard { Title = bodyTitle, Content = content };
 
             var skillResponse = new SkillResponse();

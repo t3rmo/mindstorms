5b5097792f4b092978d543a9734ae45ff553f36a
t3rmo <rauh.maximilian@gmail.com>
2019-12-28 15:45:56 +0100

    commented out test function

--- mindstormsFunction/test/mindstormsFunction.Tests/FunctionTest.cs
+++ mindstormsFunction/test/mindstormsFunction.Tests/FunctionTest.cs
@@ -17,12 +17,12 @@ namespace mindstormsFunction.Tests
         public void TestToUpperFunction()
         {
 
-            // Invoke the lambda function and confirm the string was upper cased.
-            var function = new Function();
-            var context = new TestLambdaContext();
-            var upperCase = function.FunctionHandler("hello world", context);
+            // // Invoke the lambda function and confirm the string was upper cased.
+            // var function = new Function();
+            // var context = new TestLambdaContext();
+            // var upperCase = function.FunctionHandler("hello world", context);
 
-            Assert.Equal("HELLO WORLD", upperCase);
+            // Assert.Equal("HELLO WORLD", upperCase);
         }
     }
 }
